bdfc8cd2148631750cd5bb575db5f691fadf5086
Tanusri Ghosh <tghosh25@gmail.com>
2023-02-09 22:34:01 +0530

    Add controllers files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.includes(:posts).find(params[:user_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    if @post.save
+      redirect_to user_path(current_user.id), notice: 'Post saved'
+    else
+
+      render :new, alert: 'An error occured'
+    end
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @posts = Post.find(params[:id])
+    @comments = @posts.comments.includes([:author])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author, :title, :text)
+  end
+end
